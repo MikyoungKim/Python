f81b04f056684e96f215cb095b1e145fb59aef34
MikyoungKim <50565654+MikyoungKim@users.noreply.github.com>
2019-08-30 16:41:07 +0900

    190830_py_test
    python basics test

---
+++ test1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 13:52:16 2019
+
+@author: kimmi
+"""
+
+#%%
+a = 1
+print(a)
---
+++ test3.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 14:43:01 2019
+
+@author: kimmi
+"""
+
+#%%
+# 안녕을 출력해봅시다. 
+print('안녕')
+
+#%%
+# 안녕 + 하세요를 출력
+a = ('안녕')
+b = ('하세요')
+print(a+b)
+
+#%%
+print('안녕' + '하세요')
+
+#%%
+print('안녕','하세요')
+
+#%%
+print('1 + 4')
+
+#%%
+print(1+4)
+
+#%%
+print(1/4)
+
+#%%
+print('5'/3)
+
+#%%
+print('5' + 3)
+
+#%%
+print('5'+'3')
+
+#%%
+print('5' * 3)
+
+#%%
+a = '5'
+print(a)
+b = '김치'
+milk = '우유'
+_car = 153
+
+print(milk)
+
+a = 33
+print(a)
+
+#%%
+# 변수명으로 class, if, while, def 는 사용할 수 없으므로 _를 붙여야한다. 
+_class
+
+#%% 
+# 변수명으로 class명 뿐만 아니라 숫자로도 시작할 수 없다. 
+1a = 1
+print(1a)
+
+#%%
